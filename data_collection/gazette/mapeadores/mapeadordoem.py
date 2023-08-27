dfe05962670e1fa1e610663a81eeb1ad23e2775d
trevineju <julianabtrevine@gmail.com>
2023-08-27 15:03:34 -0300

    Adiciona mapeador DOEM

--- data_collection/gazette/mapeadores/mapeadordoem.py
+++ data_collection/gazette/mapeadores/mapeadordoem.py
@@ -4,12 +4,8 @@ from gazette.mapeadores.base.mapeador import Mapeador
 class MapeadorDoem(Mapeador):
     name = "mapeadordoem"
 
-    custom_settings = {
-        "CONCURRENT_REQUESTS": 100,
-    }
-
     def column(self):
-        return "DOEM_URL"
+        return "DOEM"
 
     def backup_column(self):
         return "VALID_DOEM"
@@ -26,5 +22,6 @@ class MapeadorDoem(Mapeador):
             if "está Indisponível" not in response.text:
                 if "Não foi possível carregar o diário" not in response.text:
                     if "404 - Página não encontrada" not in response.text:
-                        return True
+                        if "ibdm.org.br" not in response.url:
+                            return True
         return False
