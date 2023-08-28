0c7ba0c64505f41d00cb1ef4c9f746201eb1a35d
trevineju <julianabtrevine@gmail.com>
2023-08-28 00:15:39 -0300

    Adiciona mapeador SIASP

--- data_collection/gazette/mapeadores/mapeadorsiasp.py
+++ data_collection/gazette/mapeadores/mapeadorsiasp.py
@@ -4,21 +4,18 @@ from gazette.mapeadores.base.mapeador import Mapeador
 class MapeadorSiasp(Mapeador):
     name = "mapeadorsiasp"
 
-    custom_settings = {
-        "CONCURRENT_REQUESTS": 50,
-    }
-
     def column(self):
-        return "SIASP_URL"
+        return "SIASP"
 
     def backup_column(self):
         return "VALID_SIASP"
 
     def urls_pattern(self, protocol, city, state_code):
         # casos conhecidos
-        # https://www.taboleirogrande.rn.gov.br/diario.php
+        # https://www.taboleirogrande.rn.gov.br/diariolista.php
+        # https://www.joaodias.rn.gov.br/diariolista.php
 
-        return [f"{protocol}://www.{city}.{state_code}.gov.br/diario.php"]
+        return [f"{protocol}://www.{city}.{state_code}.gov.br/diariolista.php"]
 
     def validation(self, response):
         if "siasp.com.br" in response.text:
