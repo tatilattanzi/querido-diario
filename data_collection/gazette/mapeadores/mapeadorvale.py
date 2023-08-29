31a6311835bf768dc5b6490248352137a2200e1e
trevineju <julianabtrevine@gmail.com>
2023-08-29 01:00:22 -0300

    Adiciona mapeador VALE

--- data_collection/gazette/mapeadores/mapeadorvale.py
+++ data_collection/gazette/mapeadores/mapeadorvale.py
@@ -4,10 +4,8 @@ from gazette.mapeadores.base.mapeador import Mapeador
 class MapeadorVale(Mapeador):
     name = "mapeadorvale"
 
-    custom_settings = {"CONCURRENT_REQUESTS": 50}
-
     def column(self):
-        return "VALE_URL"
+        return "VALE"
 
     def backup_column(self):
         return "VALID_VALE"
@@ -18,13 +16,12 @@ class MapeadorVale(Mapeador):
         # https://dom.novaolinda.to.gov.br/
 
         return [
-            f"{protocol}://www.diario.{city}.{state_code}.gov.br/",
+            f"{protocol}://diario.{city}.{state_code}.gov.br/",
             f"{protocol}://dom.{city}.{state_code}.gov.br/",
         ]
 
     def validation(self, response):
         if "Termos de uso |" in response.text:
-            if "Mantido por" in response.text:
-                if "vale-solucoes" in response.text:
-                    return True
+            if "vale-solucoes" in response.text:
+                return True
         return False
